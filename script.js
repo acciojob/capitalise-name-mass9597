fe369c7ea9ce3aef2a27c9e6e40e2b1994ccb863
Vicky@Developer <91041044+mass9597@users.noreply.github.com>
2025-07-13 07:49:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+
+const input = document.getElementById("fname");
+
+function convertToUpperCase{
+	input.value = input.value.toUpperCase();
+}
+
+input.addEventListener("blur",convertToUpperCase);
+
+
+
+
+
+
